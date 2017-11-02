cc6dd1ba33c85bb1180842da50a5e2feff720b29
Jason K Stafford <jason.stafford@scrycollective.com>
2017-11-02 09:32:58 -0700

    rework middleware after examining express-jwt middleware

--- server/middleware/auth-check.js
+++ server/middleware/auth-check.js
@@ -1,47 +1,46 @@
-const jwt = require('jsonwebtoken');
-const User = require('mongoose').model('User');
-const config = require('../config');
-
+const jwt = require('jsonwebtoken')
+const set = require('lodash/set')
+const config = require('../config')
 
 /**
  *  The Auth Checker middleware function.
+ *  Inspired by express-jwt at:
+ *  https://github.com/auth0/express-jwt/blob/master/lib/index.js
  */
 const authCheck = (req, res, next) => {
-  if (req.method === 'OPTIONS') {
-    return next();
+
+  if (req.method === 'OPTIONS' && req.headers.hasOwnProperty('access-control-request-headers')) {
+    const authorizationIndex = req.headers['access-control-request-headers']
+                                  .split(',')
+                                  .map(header => (header.trim().toLowerCase()))
+                                  .indexOf('authorization')
+
+    if (authorizationIndex >= 0) {
+      return next()
+    }
   }
-  console.log(`req.method: ${req.method}`)
-  console.log(`req.headers.authorization: ${req.headers.authorization}`)
-  console.log(`req.headers: ${JSON.stringify(req.headers)}`)
-  if (!req.headers.authorization) {
-    return res.status(401).end();
-  }
 
-  // get the last part from a authorization header string like 'bearer token-value'
-  const token = req.headers.authorization.split(' ')[1];
-  console.log(`token: ${token}`)
+  if (req.headers && req.headers.authorization) {
+    const parts = req.headers.authorization.split(' ')
+    if (parts.length === 2) {
+      const scheme = parts[0]
+      const credentials = parts[1]
 
-  // decode the token using a secret key-phrase
-  return jwt.verify(token, config.jwtSecret, (err, decoded) => {
-    console.log(`err: ${err}`)
-    // the 401 code is for unauthorized status
-    if (err) { return res.status(401).end(); }
-
-    const userId = decoded.sub;
-    console.log(`userId: ${userId}`)
-
-    // check if a user exists
-    return User.findById(userId, (userErr, user) => {
-      console.log(`userErr: ${userErr}`)
-      console.log(`user: ${user}`)
-      if (userErr || !user) {
-        return res.status(401).end();
+      if (scheme.toLowerCase() === 'bearer') {
+        jwt.verify(credentials, config.jwtSecret, (err, decoded) => {
+          if (err) {
+            return res.status(401).end()
+          } else {
+            set(req, 'user', decoded)
+            return next()
+          }
+        })
       }
+    }
+  }
 
-      return next();
-    });
-  });
-};
-
+  // don't enforce credentials, since we allow anonymous access
+  return next()
+}
 
 module.exports = authCheck
--- server/routes/api.js
+++ server/routes/api.js
@@ -4,9 +4,14 @@ const express = require('express');
 const router = new express.Router();
 
 router.get('/dashboard', (req, res) => {
-  res.status(200).json({
-    message: 'You’re authorized to see this secret message.'
-  });
+  if (req.user) {
+    res.status(200).json({
+      message: `You (${JSON.stringify(req.user)}) are authorized to see this secret message.`
+    });
+
+  } else {
+    res.sendStatus(401);
+  }
 });
 
 
